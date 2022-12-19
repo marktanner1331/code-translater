42f856b44091d2778801d4f8597e33142b98b956
mark.tanner <mark.tanner@argusmedia.com>
2022-12-19 12:58:58 +0000

    initial commit

---
+++ Code Translater/Tokenizer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Code_Translater
+{
+    public unsafe class Tokenizer
+    {
+        public readonly string Code;
+        private char[] Buffer;
+        private int Pointer;
+        private int End;
+
+        public Tokenizer(string code)
+        {
+            Code = code;
+            Buffer = code.ToCharArray();
+            Pointer = 0;
+            End = Buffer.Length;
+        }
+
+        /// <summary>
+        /// reads a token matching [0-9a-zA-Z_]+
+        /// </summary>
+        /// <returns></returns>
+        public string ReadAlphaNumeric()
+        {
+            StringBuilder sb = new StringBuilder();
+            for(;Pointer != End;Pointer++)
+            {
+                char c = Buffer[Pointer];
+                if(char.IsLetterOrDigit(c) || c == '_')
+                {
+                    sb.Append(c);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Code TranslaterTests/TokenizerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Code_Translater;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_Translater.Tests
+{
+    [TestClass()]
+    public class TokenizerTests
+    {
+        [TestMethod()]
+        public void ReadAlphaNumericTest()
+        {
+            string code = "def test():";
+            Tokenizer tokenizer = new Tokenizer(code);
+            string token = tokenizer.ReadAlphaNumeric();
+            Assert.AreEqual("def", tokenizer);
+        }
+    }
+}
